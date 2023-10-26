ceaf1d72af5a6cbe8527d9dd05ceee33a30b6272
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-10-26 22:51:59 +0000

    Kafka Producer

---
+++ Projects/Deployables/KafkaProducerProject/src/main/java/com/kafka/project/producer/KafkaProducerProject/KafkaProducerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.project.producer.KafkaProducerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerProjectApplication.class, args);
+	}
+
+}
---
+++ Projects/Deployables/KafkaProducerProject/src/test/java/com/kafka/project/producer/KafkaProducerProject/KafkaProducerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.project.producer.KafkaProducerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
