0d2bac80c85bbfd12e696951f832d803ed7818a7
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-10-26 22:53:35 +0000

    Kafka Consumer

---
+++ src/test/java/com/kafka/project/consumer/KafkaConsumerProject/KafkaConsumerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.project.consumer.KafkaConsumerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
