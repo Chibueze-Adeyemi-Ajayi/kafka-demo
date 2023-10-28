b293b1f4f7575a81a073b1af9d7b80bca30e04aa
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-10-28 17:37:05 +0000

    Kafka Consumer

--- src/main/java/com/kafka/project/consumer/KafkaConsumerProject/KafkaEventListener.java
+++ src/main/java/com/kafka/project/consumer/KafkaConsumerProject/KafkaEventListener.java
@@ -8,25 +8,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaEventListener {
     @KafkaListener(topics = "reminder-topic", groupId = "consumer-pc")
-    public void listener1 (String message) {
+    public void listener1 (Object message) {
         System.out.println("==================Message from listenr 1======================");
         System.out.println(message);
         System.out.println("==============================================================");
     }
      @KafkaListener(topics = "reminder-topic", groupId = "consumer-pc")
-    public void listener2 (String message) {
+    public void listener2 (Object message) {
         System.out.println("==================Message from listenr 2======================");
         System.out.println(message);
         System.out.println("==============================================================");
     }
     @KafkaListener(topics = "reminder-topic", groupId = "consumer-pc")
-    public void listener3 (String message) {
+    public void listener3 (Object message) {
         System.out.println("==================Message from listenr 3======================");
         System.out.println(message);
         System.out.println("==============================================================");
     }
-     @KafkaListener(topics = "reminder-topic", groupId = "consumer-pc")
-    public void listener4 (String message) {
+    @KafkaListener(topics = "reminder-topic", groupId = "consumer-pc")
+    public void listener4 (Object message) {
         System.out.println("==================Message from listenr 4======================");
         System.out.println(message);
         System.out.println("==============================================================");
